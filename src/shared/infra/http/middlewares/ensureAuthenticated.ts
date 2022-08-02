c9b4ede8a1f046472f96b4d50b98ad1f59b87218
Milenacosta08 <milena.gondim.costa08@aluno.ifce.edu.br>
2022-08-02 19:24:33 -0300

    refresh token apenas parar gerar novo token

--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -11,7 +11,6 @@ interface IPayload {
 
 export default async function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
     const authHeader = request.headers.authorization;
-    const userTokensRepository = new UsersTokenRepository();
 
     if (!authHeader) {
         throw new AppError("Token is missing", 401);
@@ -20,13 +19,7 @@ export default async function ensureAuthenticated(request: Request, response: Re
     const [, token] = authHeader.split(" ");
 
     try {
-        const { sub: user_id } = verify(token, auth.secret_refresh_token) as IPayload;
-
-        const user = await userTokensRepository.findByUserIdAndRefreshToken(user_id, token);
-
-        if (!user) {
-            throw new AppError("User does not exists", 401);
-        }
+        const { sub: user_id } = verify(token, auth.secret_token) as IPayload;
 
         request.user = {
             id: user_id
